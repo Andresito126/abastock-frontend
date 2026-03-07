86e3f1f91329fa4b5870d2928ea811035ac76147
Kevinjr2912 <233372@ids.upchiapas.edu.mx>
2026-03-06 19:40:05 -0600

    [FEAT]: add add login view (components, screen, ...)

--- app/src/main/java/com/softgenix/abastock/core/ui/theme/Color.kt
+++ app/src/main/java/com/softgenix/abastock/core/ui/theme/Color.kt
@@ -9,3 +9,30 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Paleta principal
+val NavyDark   = Color(0xFF0F1C2E)
+val NavyMid    = Color(0xFF1E3A5F)
+val NavyLight  = Color(0xFF2A5298)
+
+val AccentGold = Color(0xFFF5A623)
+
+// Backgrounds
+val Surface    = Color(0xFFF7F8FA)
+val CardBg     = Color(0xFFFFFFFF)
+val InputBg    = Color(0xFFF0F2F7)
+val IconBg     = Color(0xFFF9FAFB)
+
+// Textos
+val TextPrim   = Color(0xFF303641)
+val TextSec    = Color(0xFF8E97A8)
+val PlaceholderC = Color(0xFF9FA3A9)
+
+// Bordes y divisores
+val InputBorder = Color(0xFFE2E6EE)
+val BorderColor = Color(0xFFE5E7EB)
+val DividerClr  = Color(0xFFE5E9F0)
+val FocusBorder = Color(0xFF1E3A5F)
+
+// Iconos
+val IconTint = Color(0xFF9CA3AF)
